317fcf35f2e8479aabe3d799b51f10ebe30f1db4
Enovaid <180103167@stu.sdu.edu.kz>
2021-06-02 01:36:53 +0600

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Айдана on 02.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
